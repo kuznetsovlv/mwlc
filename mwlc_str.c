e71610eac0a228a69879dd2292267e8fca21ee46
Leonid Kuznetov <kuznetsovlv1981@gmail.com>
2019-03-23 20:36:31 +0300

    Added program broking at memory problems

--- mwlc_str.c
+++ mwlc_str.c
@@ -8,6 +8,7 @@ char *concat(const char *str1, const char *str2)
 	if(!(result  = malloc(sizeof(char) * (strlen(str1) + strlen(str2) + 1))))
 	{
 		fprintf(stderr, "Can not allocate memory.");
+		exit(-2);
 	}
 	strcpy(result, str1);
 
